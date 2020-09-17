6a8f3eaa1966edb28c2994651525a821f25089b8
Kenneth Cheng <cheng.ky@hotmail.com>
2020-09-17 02:45:15 -0400

    add lee.smallbone()

--- R/exprs.R
+++ R/exprs.R
@@ -3,7 +3,7 @@
 
 ### --- GIMME and E-Fmin --- ### 
 
-gimme <- function(model, expr, rmfs="biomass", lbs=0.01, relative=FALSE, norm=TRUE, cutoff=1, mode=c(0,1), gap=NULL, agap=NULL, solv.pars=list(), samp.pars=NULL) {
+gimme <- function(model, expr, rmfs="biomass", lbs=0.01, relative=FALSE, norm=TRUE, cutoff=1, mode=c(0,1), nc=1L, gap=NULL, agap=NULL, solv.pars=list(), samp.pars=NULL) {
   # function for running GIMME or E-Fmin; by default will run E-Fmin
   # expr is a vector of gene expression values (may be pre-normalized), either with names as gene symbols as in model$genes, or if unnamed the length and order should correspond to model$genes
   # rmfs: reaction indices or IDs (as in model$rxns) for "required metabolic functions"; or regex for biomass reaction; or NULL (if want to manually add constraints wrt rmf prior to calling this function)
@@ -11,7 +11,7 @@ gimme <- function(model, expr, rmfs="biomass", lbs=0.01, relative=FALSE, norm=TR
   # norm: whether to scale and shift the reaction expression so that they are within [0,1], default TRUE corresponds to E-Fmin
   # cutoff: gene expression cutoff above which the weight is 0 for GIMME; default 1 corresponds to E-Fmin
   # mode 0 means solve the model once and return list(gimme.model, fluxes) where fluxes is the vector of optimal fluxes;
-  # mode 1 means first get optimal objective value, then constrain obj at optimal or near optimal (as defined by gap and agap), then run FVA, and return list(gimme.model, result.model, fluxes) where result.model is updated model with rxn bounds set to the FVA output
+  # mode 1 means first get optimal objective value, then constrain obj at optimal or near optimal (as defined by gap and agap), then run FVA with n.cores=nc, and return list(gimme.model, result.model, fluxes) where result.model is updated model with rxn bounds set to the FVA output
   # when mode==1 and samp.pars not NULL, will sample the resulting model
   
   mode <- match.arg(as.character(mode[1]), c("0","1"))
@@ -45,7 +45,7 @@ gimme <- function(model, expr, rmfs="biomass", lbs=0.01, relative=FALSE, norm=TR
   	v <- solv.res$xopt[1:length(gimme.model$rxns)]
   	res <- list(gimme.model=gimme.model, fluxes=v)
   } else if (mode=="1") {
-  	solv.res <- fva1(gimme.model, gap=gap, agap=agap, keep.solv.out=TRUE, solv.pars=solv.pars)
+  	solv.res <- fva1(gimme.model, nc=nc, gap=gap, agap=agap, keep.solv.out=TRUE, solv.pars=solv.pars)
   	gimme.model$solver.out <- solv.res$solver.out
   	v <- solv.res$solver.out$xopt[1:length(gimme.model$rxns)]
   	res.model <- model
@@ -77,8 +77,8 @@ form.gimme <- function(model, w, rmfs, rmf.lbs) {
   )
   rowlb <- c(model$rowlb, rep(0, n))
   rowub <- c(model$rowub, rep(0, n))
-  lb <- c(model$lb, rep(0, n))
-  ub <- c(model$ub, rep(Inf, n))
+  lb <- c(model$lb, rep(0, 2*n))
+  ub <- c(model$ub, rep(Inf, 2*n))
   c <- c(rep(0, n.rxns), w, w)
   vtype <- rep("C", length(c))
 
@@ -104,106 +104,67 @@ form.gimme <- function(model, w, rmfs, rmf.lbs) {
 
 ### --- Lee et al. BMC Syst Biol 2012 --- ### 
 
-lee.smallbone <- function(model, expr, rmfs="biomass", lbs=0.01, relative=FALSE, norm=TRUE, cutoff=1, mode=c(0,1), gap=NULL, agap=NULL, solv.pars=list(), samp.pars=NULL) {
+lee.smallbone <- function(model, expr, weights=1, mode=c(0,1), nc=1L, gap=NULL, agap=NULL, solv.pars=list(), samp.pars=NULL) {
   # function for running the algorithm of Lee et al., the function name is from the surnames of the two co-first authors
-  # expr is a vector of gene expression values (may be pre-normalized), either with names as gene symbols as in model$genes, or if unnamed the length and order should correspond to model$genes
-  # rmfs: reaction indices or IDs (as in model$rxns) for "required metabolic functions"; or regex for biomass reaction; or NULL (if want to manually add constraints wrt rmf prior to calling this function)
-  # lbs: minimal fluxes for rmfs in the corresponding order, or a single value; if relative=TRUE, lbs will be treated as the fraction of the maximal flux values; the default correspond to what's used in the E-Fmin paper
-  # norm: whether to scale and shift the reaction expression so that they are within [0,1], default TRUE corresponds to E-Fmin
-  # cutoff: gene expression cutoff above which the weight is 0 for GIMME; default 1 corresponds to E-Fmin
-  # mode 0 means solve the model once and return list(gimme.model, fluxes) where fluxes is the vector of optimal fluxes;
-  # mode 1 means first get optimal objective value, then constrain obj at optimal or near optimal (as defined by gap and agap), then run FVA, and return list(gimme.model, result.model, fluxes) where result.model is updated model with rxn bounds set to the FVA output
+  # expr is a vector of non-negative gene expression values (may be pre-normalized), either with names as gene symbols as in model$genes, or if unnamed the length and order should correspond to model$genes
+  # mode 0 means solve the model once and return list(ls.model, fluxes) where fluxes is the vector of optimal fluxes;
+  # mode 1 means first get optimal objective value, then constrain obj at optimal or near optimal (as defined by gap and agap), then run FVA (with n.cores=nc), and return list(ls.model, result.model, fluxes) where result.model is updated model with rxn bounds set to the FVA output
   # when mode==1 and samp.pars not NULL, will sample the resulting model
   
   mode <- match.arg(as.character(mode[1]), c("0","1"))
-  if (length(rmfs)==1 && grepl("biomass", rmfs, ignore.case=TRUE)) {
-    rxns <- get.biomass.idx(model, rgx=rmfs)
-  } else rxns <- all2idx(model, rmfs)
 
   # process expression values
   x <- exprs2fluxes(model, expr, na.after=NA)
-  if (norm) {
-  	xmin <- min(x, na.rm=TRUE)
-  	xmax <- max(x, na.rm=TRUE)
-  	if (xmin<0) x <- (x-xmin)/xmax else x <- x/xmax # original E-Fmin simply used x/xmax, seeming to assume that all expression values are positive and zero means no expression
+  if (length(weights)==1) w <- rep(weights, length(x))
+
+  # form model and solve iteratively
+  nr <- which(model$lb>=0)
+  repeat {
+  	ls.model <- form.lee.smallbone(model, nr, x[nr], w[nr])
+  	solv.res <- fva1(ls.model, rxns=setdiff(1:length(model$rxns), nr), nc=nc, keep.solv.out=TRUE, solv.pars=solv.pars)
+  	nr.new <- solv.res$fva.res[vmin>=0,id]
+  	if (length(nr.new)==0) break else nr <- c(nr, nr.new)
   }
-  w <- ifelse(x<cutoff, cutoff-x, 0)
-  w[is.na(w)] <- 0
-
-  # set the lower bounds of required reactions; for the reversible reactions, the constraints of |v|>lbs need to be handled with interger programming later
-  if (!is.null(rmfs)) {
-  	if (length(lbs)==1) lbs <- rep(lbs, length(rxns))
-    if (relative) lbs <- lbs * get.opt.fluxes(model, rxns, "max")
-    isrev <- model$lb[rxns] < -lbs
-    model$lb[rxns[!isrev]] <- lbs[!isrev]
-    rxns <- rxns[isrev]
-    lbs <- lbs[isrev]
-  } else rxns <- lbs <- NULL
-
-  # form model
-  gimme.model <- form.gimme(model, w, rxns, lbs)
-
+  
   # solve model and extract results
-  if (any(isrev)) solv.pars <- get.pars("mip", solv.pars) else solv.pars <- get.pars("lp", solv.pars)
   if (mode=="0") {
-  	solv.res <- solve.model(gimme.model, pars=solv.pars)[[1]]
-  	gimme.model$solver.out <- solv.res
-  	v <- solv.res$xopt[1:length(gimme.model$rxns)]
-  	res <- list(gimme.model=gimme.model, fluxes=v)
+  	ls.model$solver.out <- solv.res$solver.out
+  	v <- solv.res$solver.out$xopt[1:length(ls.model$rxns)]
+  	res <- list(ls.model=ls.model, fluxes=v)
   } else if (mode=="1") {
-  	solv.res <- fva2(gimme.model, gap=gap, agap=agap, keep.solv.out=TRUE, solv.pars=solv.pars)
-  	gimme.model$solver.out <- solv.res$solver.out
-  	v <- solv.res$solver.out$xopt[1:length(gimme.model$rxns)]
+  	solv.res <- fva1(ls.model, nc=nc, gap=gap, agap=agap, keep.solv.out=TRUE, solv.pars=solv.pars)
+  	ls.model$solver.out <- solv.res$solver.out
+  	v <- solv.res$solver.out$xopt[1:length(ls.model$rxns)]
   	res.model <- model
   	res.model$lb <- solv.res$fva.res$vmin
   	res.model$ub <- solv.res$fva.res$vmax
   	if (!is.null(samp.pars)) {
   	  res.model <- sample.model(res.model, samp.pars)
   	}
-  	res <- list(gimme.model=gimme.model, result.model=res.model, fluxes=v)
+  	res <- list(ls.model=ls.model, result.model=res.model, fluxes=v)
   }
   
   res
 }
 
-form.gimme <- function(model, w, rmfs, rmf.lbs) {
-  # formulate a GIMME-like model
+form.lee.smallbone <- function(model, idx, x, w) {
+  # formulate the model of Lee et al.
 
-  n.mets <- nrow(model$S)
   n.rxns <- ncol(model$S)
 
-  # formulating the minimization of weighted sum of absolute values \Sum w|v|, by replacing v with two slack variables v1, v2 such that v=v1-v2, then |v| can be expressed as v1+v2
-  # however, I still keep v in the formulation because it makes it easier to run FVA after initially solving the model, if needed
-  rxns1 <- which(w!=0 & !is.na(w))
-  w <- w[rxns1]
-  n <- length(rxns1)
+  # formulating the minimization of weighted sum of absolute values \Sum w|v-x|, by adding two slack variables v1, v2 such that v-x=v1-v2, then |v-x| can be expressed as v1+v2
+  n <- length(idx)
   S <- rbind(
-    cbind( model$S,                          sparseMatrix(NULL, NULL, dims=c(n.mets, 2*n)) ),
-    cbind( sparseMatrix(1:n, rxns1, x=1, dims=c(n, n.rxns)),  Diagonal(n,-1),  Diagonal(n) )
+    cbind( model$S,                        sparseMatrix(NULL, NULL, dims=c(n.mets, 2*n)) ),
+    cbind( sparseMatrix(1:n, idx, x=1, dims=c(n, n.rxns)),  Diagonal(n,-1),  Diagonal(n) )
   )
-  rowlb <- c(model$rowlb, rep(0, n))
-  rowub <- c(model$rowub, rep(0, n))
-  lb <- c(model$lb, rep(0, n))
-  ub <- c(model$ub, rep(Inf, n))
+  rowlb <- c(model$rowlb, x)
+  rowub <- c(model$rowub, x)
+  lb <- c(model$lb, rep(0, 2*n))
+  ub <- c(model$ub, rep(Inf, 2*n))
   c <- c(rep(0, n.rxns), w, w)
-  vtype <- rep("C", length(c))
-
-  # handling |v|>lb constraints, if any, by adding an binary variable y and the constraint lb <= v + My <= M-lb, where M is a large constant, here I used M=1e4
-  n.rmfs <- length(rmfs)
-  if (n.rmfs>0) {
-	S <- rbind(
-	  cbind( S,                            sparseMatrix(NULL, NULL, dims=c(nrow(S), n.rmfs)) )
-	  cbind( sparseMatrix(1:n.rmfs, rmfs, dims=c(n.rmfs, ncol(S))), Diagonal(n.rmfs, x=1e4) )
-  	)
-  	rowlb <- c(rowlb, rmf.lbs)
-  	rowub <- c(rowub, 1e4-rmf.lbs)
-  	lb <- c(lb, rep(0, n.rmfs))
-  	ub <- c(ub, rep(1, n.rmfs))
-  	c <- c(c, rep(0, n.rmfs))
-  	vtype <- c(vtype, rep("I", n.rmfs))
-  }
 
   # return model
-  list(rxns=model$rxns, mets=model$mets, csense="min", c=c, S=S, rowlb=rowlb, rowub=rowub, lb=lb, ub=ub, vtype=vtype)
+  list(rxns=model$rxns, mets=model$mets, csense="min", c=c, S=S, rowlb=rowlb, rowub=rowub, lb=lb, ub=ub)
 }
 
