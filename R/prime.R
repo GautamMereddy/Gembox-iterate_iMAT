a9002400a0756be79ca5a8d32c782599e272d8c0
Kenneth Cheng <cheng.ky@hotmail.com>
2020-09-27 22:02:37 -0400

    reorganize prime functions to facilitate tuning

--- R/prime.R
+++ R/prime.R
@@ -2,7 +2,7 @@
 
 
 prime <- function(model, expr, gr, padj.cutoff=0.05, nc=1L, bm.rgx="biomass", default.bnd=1e3, bm.epsil=1e-4, min.step.size=0.1, bm.lb.rel=0.1, solv.pars=list()) {
-  # function to run PRIME
+  # function to run PRIME all steps at once
   # expr: a gene-by-sample matrix, need to has rownames of gene symbols as those in model$genes (but doesn't need to be of same length or in order)
   # gr: cell growth rate measures across samples, corresponding to the order or columns of expr
   # padj.cutoff: used to define significant growth-associated rxns
@@ -11,20 +11,75 @@ prime <- function(model, expr, gr, padj.cutoff=0.05, nc=1L, bm.rgx="biomass", de
 
   solv.pars <- get.pars("lp", solv.pars)
   message("Preparing base model...")
+  model <- prepare.model.prime(model, default.bnd=default.bnd, bm.epsil=bm.epsil, min.step.size=min.step.size, bm.rgx=bm.rgx, solv.pars=solv.pars)
+  message("Identifying growth-associated reactions...")
+  prm.rxns <- get.prime.rxns(model, expr=expr, gr=gr, nc=nc, padj.cutoff=padj.cutoff)
+  res <- run.prime(model, prm.rxns=prm.rxns, gr=gr, nc=nc, bm.rgx=bm.rgx, bm.lb.rel=bm.lb.rel, solv.pars=solv.pars)
+}
+
+prepare.model.prime <- function(model, default.bnd=1e3, bm.epsil=1e-4, min.step.size=0.1, bm.rgx="biomass", solv.pars=get.pars("lp", list())) {
+  # step 1 of PRIME: prepare base model
+  # default.bnd, bm.epsil, min.step.size, bm.rgx passed to shrink.model.bounds()
+
   # split each reversible rxn into two forward/backward rxns
   model <- convert.rev.rxns(model)
   # shrink rxn bounds
   model <- shrink.model.bounds(model, rxns="default", default.bnd=default.bnd, bm.epsil=bm.epsil, relative=FALSE, min.step.size=min.step.size, bm.rgx=bm.rgx, solv.pars=solv.pars)
-  # find growth-associated rxns and compute their rxn-level values for each sample
-  message("Identifying growth-associated reactions...")
-  prm.rxns <- get.prime.rxns(model, expr=expr, gr=gr, nc=nc, padj.cutoff=padj.cutoff)
+}
+
+get.prime.rxns <- function(model, expr, gr, nc=1L, padj.cutoff=0.05) {
+  # step 2 of PRIME: select growth-associated rxns (i.e. rxns with significant correlation with the provided growth rate data across multiple samples)
+  # expr: a gene-by-sample matrix, need to has rownames of gene symbols as those in model$genes (but doesn't need to be of same length or in order)
+  # gr: cell growth rate measures across samples, corresponding to the order or columns of expr
+  # return list(x, cor, i), x is the all-rxn-by-sample matrix mapped from expr, cor is a data.table containing results of correlation tests, i is the indices of the resulting growth-associated rxns with padj<padj.cutoff
+  # this is a separate step and the results are returned as such to allow manual adjustment of selected reactions w/o recomputing the entire correlation
+  # manually adjust result with something like: res$i <- res$cor[padj<new.cutoff, id], then pass result to the next step
+
+  expr <- expr[match(model$genes, rownames(expr)), ]
+  if (all(is.na(expr))) stop ("expr doesn't contain any of the model genes!")
+
+  # map gene expr values to rxns values by taking the mean
+  mat <- t(sapply(model$rules, function(x) {
+  	i <- as.integer(stringr::str_extract_all(x, "[0-9]+")[[1]])
+  	colMeans(expr[i,,drop=FALSE], na.rm=TRUE)
+  }))
+  mat[is.nan(mat)] <- NA
+  if (!is.null(colnames(expr))) colnames(mat) <- colnames(expr)
+
+  # correlation between rxn values and growth rates across samples for each rxn
+  cor.res <- rbindlist(parallel::mclapply(1:nrow(mat), function(i) {
+  	tryCatch({
+  	  a <- cor.test(mat[i,], gr, method="spearman")
+  	  data.table(rho=a$estimate, pval=a$p.value)
+  	}, error=function(e) data.table(rho=NA, pval=NA))
+  }, mc.cores=nc), idcol="id")
+  cor.res[, padj:=p.adjust(pval,"BH")]
+  tmp <- sum(cor.res$padj<padj.cutoff)
+  if (tmp==0) stop("No significant growth-associated reactions with padj<", padj.cutoff, ".") else message("Found ", tmp, " growth-associated reactions with padj<", padj.cutoff, ".")
+
+  list(x=mat, cor=cor.res, i=cor.res[padj<padj.cutoff, id])
+}
+
+run.prime <- function(model, prm.rxns, gr, nc=1L, bm.rgx="biomass", bm.lb.rel=0.1, solv.pars=get.pars("lp", list())) {
+  # step 3 of PRIME (all the remaining steps)
+  # prm.rxns: output from get.prime.rxns
+  # gr: cell growth rate measures across samples, corresponding to the order or columns of mat
+  # bm.lb.res passed to get.norm.range.min()
+  # return a list of updated models, one for each sample in the corresponding order
+
   # compute the lb and ub of the normalization range
   rmin <- get.norm.range.min(model, bm.lb.rel=bm.lb.rel, nc=nc, bm.rgx=bm.rgx, solv.pars=solv.pars)
   message("Computing the upper bound of normalization range...")
   rmax <- get.norm.range.max(model, rxns=prm.rxns$i, range.min=rmin, nc=nc, bm.rgx=bm.rgx, solv.pars=solv.pars)
   # compute and set final rxn ub values for each sample
   message("Generating output models...")
-  ubs <- prm.rxns$x * (rmax-rmin) + rmin
+  mat <- prm.rxns$x[prm.rxns$i,] * prm.rxns$cor[match(prm.rxns$i, id), sign(rho)]
+  mat <- do.call(rbind, parallel::mclapply(1:nrow(mat), function(i) {
+  	x <- mat[,i]
+  	rng <- range(x, na.rm=TRUE)
+  	(x-rng[1]) / diff(rng)
+  }, mc.cores=nc))
+  ubs <- mat * (rmax-rmin) + rmin
   models <- apply(ubs, 2, function(x) {
   	m <- model
   	m$ub[prm.rxns$i] <- x
@@ -34,44 +89,6 @@ prime <- function(model, expr, gr, padj.cutoff=0.05, nc=1L, bm.rgx="biomass", de
   res <- lapply(models, revert.rev.rxns)
   message("Done.")
   res
-}
-
-get.prime.rxns <- function(model, expr, gr, nc=1L, padj.cutoff=0.05) {
-  # select growth-associated rxns for PRIME (i.e. rxns with significant correlation with the provided growth rate data across multiple samples)
-  # expr: a gene-by-sample matrix, need to has rownames of gene symbols as those in model$genes (but doesn't need to be of same length or in order)
-  # gr: cell growth rate measures across samples, corresponding to the order or columns of expr
-  # return list(i, x), i is the indices of the resulting growth-associated rxns, x is the rxn-by-sample matrix containing values that correspond to the (E-min(E))/(max(E)-min(E)) part in the Eq. (4) from Yizhak et al.
-
-  expr <- expr[match(model$genes, rownames(expr)), ]
-  if (all(is.na(expr))) stop ("expr doesn't contain any of the model genes!")
-
-  # map gene expr values to rxns values by taking the mean
-  mat <- sapply(model$rules, function(x) {
-  	i <- as.integer(stringr::str_extract_all(x, "[0-9]+")[[1]])
-  	colMeans(expr[i,,drop=FALSE], na.rm=TRUE)
-  })
-  mat[is.nan(mat)] <- NA
-
-  # correlation between rxn values and growth rates across samples for each rxn
-  cor.res <- rbindlist(parallel::mclapply(1:ncol(mat), function(i) {
-  	tryCatch({
-  	  a <- cor.test(mat[,i], gr, method="spearman")
-  	  data.table(rho=a$estimate, pval=a$p.value)
-  	}, error=function(e) data.table(rho=NA, pval=NA))
-  }, mc.cores=nc), idcol="id")
-  cor.res[, padj:=p.adjust(pval,"BH")]
-  # select rxns with signifcant correlations and compute the normalized rxn values
-  cor.res <- cor.res[padj<padj.cutoff]
-  if (nrow(cor.res)==0) stop("No significant growth-associated reactions with padj<", padj.cutoff, ".") else message("Found ", nrow(cor.res), " growth-associated reactions with padj<", padj.cutoff, ".")
-  mat <- mat[, cor.res$id] * rep(sign(cor.res$rho), each=nrow(mat))
-  mat <- do.call(rbind, parallel::mclapply(1:ncol(mat), function(i) {
-  	x <- mat[,i]
-  	rng <- range(x, na.rm=TRUE)
-  	(x-rng[1]) / diff(rng)
-  }, mc.cores=nc))
-  if (!is.null(colnames(expr))) colnames(mat) <- colnames(expr)
-
-  list(i=cor.res$id, x=mat)
 }
 
 get.norm.range.min <- function(model, bm.lb.rel=0.1, nc=1L, bm.rgx="biomass", solv.pars=get.pars("lp", list())) {
